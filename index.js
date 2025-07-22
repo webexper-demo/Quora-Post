3aefda07eb4dc50f12d40f005b78cea543c762ab
gaurav kumar <gauravkumar07606@gmail.com>
2025-07-22 09:35:35 +0530

    Added show.ejs file and setup project files

--- index.js
+++ index.js
@@ -1,52 +1,45 @@
-// const { log } = require("console");
 const express = require("express");
 const app = express();
-
 const port = 8080;
-
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+ // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.set("views engine", "ejs");
+app.set("view engine", "ejs");  // ✅ Fix here
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/post', (req, res)=>{
-    res.render("index.ejs",{posts})
-})
-app.get("/post/new", (req, res)=>{
-    res.render("new.ejs")
-})
-
-app.post("/post",(req,res)=>{
-    
-    let {username, content} = req.body;
-    posts.push({username, content});
-    res.redirect("/post");
-    
-})
-
 let posts = [
-    {
-        username: 'shakti ',
-        content: " i love  coding",
-        
-    },
-    {
-        username: 'gaurv kuamr',
-        content: " i love  coding",
+    { id: uuidv4(), username: 'shakti', content: "i love coding" },
+    { id: uuidv4(), username: 'gaurv kuamr', content: "i love coding" },
+    { id: uuidv4(), username: 'ashutosh', content: "i love coding" }
+];
 
-    },
-    {
-        username: 'ashutosh',
-        content: " i love  coding",
+app.get('/post', (req, res) => {
+    res.render("index.ejs", { posts });
+});
 
-    }
+app.get("/post/new", (req, res) => {
+    res.render("new.ejs");
+});
 
-]
-app.listen(port, ()=>{
-    console.log("listenign to port : 8080");
-    
-})
+app.post("/post", (req, res) => {
+    let { username, content } = req.body;
+    let id = uuidv4();
+    posts.push({ id, username, content }); // added id
+    res.redirect("/post");
+});
+
+app.get("/post/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);  // ✅ fix here
+    console.log(post);
+    res.render("show.ejs", { post});
+});
+
+app.listen(port, () => {
+    console.log("Listening on port: 8080");
+});
